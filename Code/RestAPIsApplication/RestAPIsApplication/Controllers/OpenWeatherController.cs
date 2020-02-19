1d27e04c14810ff8f955c057bec3f9c827f289ea
CNLjunggren <35855679+CNLjunggren@users.noreply.github.com>
2020-02-18 17:16:43 -0700

    NuGet Package Updates & Basic Webpages
    - Nuget Packages for Bootstrap and jQuery have been updated to the latest stable updates.
    - Basic controller created for handling all webpage actions for the OpenWeather part of the website.
    - Basic webpages created for the OpenWeather and Home pages of the web application.
    - Layout of views modified to have partial view support for the header and footer for each webpage.
    - Navigation bar added to the header.

---
+++ Code/RestAPIsApplication/RestAPIsApplication/Controllers/OpenWeatherController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestAPIsApplication.Controllers
+{
+    public class OpenWeatherController : Controller
+    {
+        // TODO: Private instanciation of the APIService business service.
+        
+
+        /// <summary>
+        ///     Current Weather Page.
+        /// </summary>
+        /// <returns>View()</returns>
+        public ActionResult OpenWeather()
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///     Http Post method responsible for calling the business service responsible for API calls to OpenWeather in regards to Current Weather requests.
+        /// </summary>
+        /// <returns>View()</returns>
+        public ActionResult GetCurrent()
+        {
+            // TODO: Add functionality to call the OpenWeather API and make a Current Weather request based off the user input.
+            return RedirectToAction("OpenWeather", "OpenWeather");
+        }
+    }
+}
